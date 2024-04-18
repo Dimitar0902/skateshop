16c4f44b2d0acc70d952f9030995dde37154ba41
Dimitar0902 <133356778+Dimitar0902@users.noreply.github.com>
2024-04-18 09:59:55 +0200

    Finished the basic navbar design and functionality

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Logo from '../images/logo.png'
 import {Link} from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 import '../styles/Navbar.css'
 
 function Navbar() {
+
+  const [openLinks, setOpenLinks] = useState(false)
+  const toggleNavbar = () =>{
+    setOpenLinks(!openLinks);
+  }
+
   return (
     <div className='navbar'>
-      <div className='left'>
+      <div className='left' id={openLinks ? "open" : "close"}>
         <img src={Logo} alt='Logo'/>
+        <div className="hiddenLinks">
+         <Link to="/"> Home </Link>
+         <Link to="/about"> About </Link>
+         <Link to="/shop"> Shop </Link>
+         <Link to="/contact"> Contact </Link>
+        </div>
       </div>
       <div className='right'>
         <Link to="/"> Home </Link>
         <Link to="/about"> About </Link>
         <Link to="/shop"> Shop </Link>
         <Link to="/contact"> Contact </Link>
+        <button onClick={toggleNavbar} >
+          <FontAwesomeIcon icon={faBars} />
+        </button>
       </div>
     </div>
   )
