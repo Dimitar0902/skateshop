5dd04785139d825b313d16b0021d75a7a9a43e38
Dimitar0902 <133356778+Dimitar0902@users.noreply.github.com>
2024-05-03 15:17:07 +0200

    Small styling changes and adding functionality to contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,32 +1,44 @@
-import React from 'react'
-import contactImg from '../images/contactImg.jpg'
-import '../styles/Contact.css'
+import React, { useRef } from 'react';
+import emailjs from 'emailjs-com';
+import contactImg from '../images/contactImg.jpg';
+import '../styles/Contact.css';
 
 function Contact() {
+  const formRef = useRef();
+
+  function sendEmail(e) {
+    e.preventDefault();
+
+    emailjs.sendForm('service_hm1ouxt', 'template_zukg4i1', formRef.current, 'cmnp_z7hvQERzhs1s')
+      .then((result) => {
+        console.log(result.text);
+        
+      }, (error) => {
+        console.log(error.text);
+        
+      });
+  }
+
   return (
     <div className='contact'>
-      <div className='leftSide' style={{ backgroundImage: `url(${contactImg})`}}>
-
-      </div>
+      <div className='leftSide' style={{ backgroundImage: `url(${contactImg})`}}></div>
       <div className='rightSide'>
-            <h1>Contact Us</h1>
-            <form id='contact-form' method="POST">
+        <h1>Contact Us</h1>
+        <form id='contact-form' ref={formRef} onSubmit={sendEmail}>
+          <label htmlFor='name'>Full Name</label>
+          <input name='name' placeholder='Enter Full Name' type='text'/>
 
-            <label htmlFor='name'>Full Name</label>
-            <input name='name' placeholder='Enter Full Name' type='text'/>
+          <label htmlFor='email'>Email</label>
+          <input name='email' placeholder='Enter Email' type='email'/>
 
-             <label htmlFor='email'>Email</label>
-            <input name='email' placeholder='Enter Email' type='email'/>
+          <label htmlFor='message'>Message</label>
+          <textarea rows="6" placeholder='Enter Message' name='message' required></textarea>
 
-             <label htmlFor='message'>Message</label>
-            <textarea rows="6" placeholder='Enter Message' name='message' required> </textarea>
-
-            <button type='submit'>Send Message</button>
-
-            </form>
+          <button type='submit'>Send Message</button>
+        </form>
       </div>
     </div>
-  )
+  );
 }
 
-export default Contact
+export default Contact;
